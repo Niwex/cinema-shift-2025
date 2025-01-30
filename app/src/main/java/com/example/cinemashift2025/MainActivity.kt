d8c119e83eee7b5b46704b6b5161b596a0d84953
Chelpanov Daniil <d.chelpanov.work@gmail.com>
2025-01-30 14:12:18 +0300

    почистил импорты

--- app/src/main/java/com/example/cinemashift2025/MainActivity.kt
+++ app/src/main/java/com/example/cinemashift2025/MainActivity.kt
@@ -4,13 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.cinemashift2025.ui.theme.CinemaShift2025Theme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/example/cinemashift2025/addiche/di/AfficheModule.kt
+++ app/src/main/java/com/example/cinemashift2025/addiche/di/AfficheModule.kt
@@ -1,16 +1,13 @@
 package com.example.cinemashift2025.addiche.di
 
 import com.example.cinemashift2025.addiche.data.network.AfficheApi
-import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.dsl.factoryOf
-import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import com.example.cinemashift2025.addiche.domain.usecase.GetFilmItemsUseCase
 import com.example.cinemashift2025.addiche.domain.repository.AfficheRepository
 import com.example.cinemashift2025.addiche.presentation.AfficheViewModel
 import com.example.cinemashift2025.addiche.data.repository.AfficheRepositoryImpl
-import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModel
 
 val afficheModule = module {
